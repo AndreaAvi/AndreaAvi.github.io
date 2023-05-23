76a2cdbb4e304b6a3b3c7ce11f9d051e08f1c225
Andrea Cáceres <98946637+AndreaAvi@users.noreply.github.com>
2023-05-22 22:04:48 -0600

    Se actualizaron todos los archivos

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('Tengo una pasión por la tecnología y la programación.')
   .pauseFor(200)
   .deleteChars(10)
   .start();
